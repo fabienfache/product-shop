a5a145e20570e3a1a5e9c4c85b78fad8b33ce146
Fabien Fache <fabienfache@hotmail.com>
2023-07-28 15:02:38 +0200

    ajout de quelques tests d'integration sur le controlleur

---
+++ src/test/java/com/atlen/productshop/controller/ProductControllerIntegrationTest.java
@@ -0,0 +1,63 @@
+package com.atlen.productshop.controller;
+
+import com.atlen.productshop.model.ProductDto;
+import com.atlen.productshop.service.ProductService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ProductsApiController.class)
+
+public class ProductControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ProductService productService;
+
+    @Test
+    public void shouldReturnAllProducts() throws Exception {
+
+        List<ProductDto> productDtoList = List.of(ProductDto.builder().id(1L).code("fhgjfg").name("test1").build());
+
+        given(productService.getAllProducts()).willReturn(productDtoList);
+
+        mvc.perform(get("/products")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].code", is(productDtoList.get(0).getCode())));
+
+    }
+
+    @Test
+    public void shouldExpectNotFoundWhenGetProductbyID() throws Exception {
+
+        mvc.perform( MockMvcRequestBuilders
+                        .get("/employees/{id}", 1)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+}
